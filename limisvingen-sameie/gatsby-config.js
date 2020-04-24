c0ab38c3848f987519d4fbdefe1208e4927ce904
Anders Skifte <anders@skifte.com>
2020-04-24 23:49:51 +0200

    Metadata

--- limisvingen-sameie/gatsby-config.js
+++ limisvingen-sameie/gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `Limisvingen realsameie`,
+    author: `Anders Skifte`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://limisvingen.no/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `skifte`,
     },
   },
   plugins: [
