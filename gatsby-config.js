dea1f98edaa8f8931da9c23c08d13e36bdcdb747
Anders Skifte <anders.skifte@imatis.com>
2020-05-26 12:04:47 +0200

    Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-167634816-1`,
       },
     },
     {
